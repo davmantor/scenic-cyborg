84af80c431b29b217fa813d9a5baa486e9c19832
jlortiz0 <jlortiz0@users.noreply.github.com>
2024-06-07 02:47:50 -0700

    Parameterization, but no rules

--- src/scenic/simulators/cyborg/objects.py
+++ src/scenic/simulators/cyborg/objects.py
@@ -1,5 +1,5 @@
 from typing import Any
-from collections import namedtuple
+from collections import defaultdict, namedtuple
 
 from CybORG.Simulator.Entity import Entity
 from CybORG.Simulator.Host import Host
@@ -17,14 +17,33 @@ class CybORGObject(Object):
     def get_properties(self, sim: SimulationController) -> dict[str, Any]:
         raise NotImplementedError()
 
+# why is this not a damn enum
+# i could say that about a lot of cyborg really
 class CybORGSubnet(CybORGObject):
+    def __init__(self, cyborg_name: str, nacl: dict[str, tuple[bool]], image_map: dict[str, tuple[str]] = defaultdict(lambda: (None, None)), *args, **kwargs):
+        super().__init__(cyborg_name, *args, **kwargs)
+        # maps image names to pairs of default confidentiality and availability for that image
+        # to use whatever cyborg's default is for that (or if the value will not be queried) map to None
+        # (None, None) pairs can be omitted
+        # this can be used to create a network of unimportant servers but impatient users/confidential document holders
+        # and a network of important servers but unimportant workstations
+        # or whatever combination you can think of
+        if not isinstance(image_map, defaultdict):
+            image_map = defaultdict(lambda: (None, None), image_map)
+        self.image_map = image_map
+        self.nacl = nacl
+
     def get_cyborg_obj(self, sim: SimulationController) -> Entity:
         return sim.state.subnets[sim.state.subnet_name_to_cidr[self.cyborg_name]]
 
 class CybORGHost(CybORGObject):
-    def __init__(self, cyborg_name: str, subnet: CybORGSubnet, *args, **kwargs):
+    def __init__(self, cyborg_name: str, subnet: CybORGSubnet, image: str, confidentiality: str = None, availability: str = None, linked_hosts: list[str] = [], *args, **kwargs):
         super().__init__(self, cyborg_name, *args, **kwargs)
         self.subnet = subnet
+        self.image = image
+        self.confidentiality = confidentiality or subnet.image_map[image][0]
+        self.availability = availability or subnet.image_map[image][1]
+        self.linked_hosts = linked_hosts
 
     def get_cyborg_obj(self, sim: SimulationController) -> Host:
         return sim.state.hosts[self.cyborg_name]
@@ -47,5 +66,7 @@ class CybORGAgent(CybORGObject):
 # or a thing to make one green agent for each "user" host
 
 class CybORGEgoAgent(CybORGAgent):
-    def __init__(self, *args, **kwargs):
+    # TODO artifacts should probably not be empty
+    def __init__(self, artifacts: list[str] = [], *args, **kwargs):
         super().__init__("Blue", None, *args, **kwargs)
+        self.artifacts = artifacts
--- src/scenic/simulators/cyborg/simulator.py
+++ src/scenic/simulators/cyborg/simulator.py
@@ -17,49 +17,51 @@ class CybORGSimulator(Simulator):
         return CybORGSimulation(scene, SimulationController(scenario), **kwargs)
 
     def _initialize_subnet(self, subnet: CybORGSubnet):
-        subnetData = {"Size": 0, "Hosts": []}
-        # TODO NACLs
-        # Also special handling for defender and attacker subnets, if this game uses them
+        subnetData = {"Size": 0, "Hosts": [], "NACLs": {}}
+        # TODO Support port-specific NACLs? For now only all NACLs are supported for simplicity
+        for k, v in subnet.nacls:
+            subnetData["NACLs"][k] = {"in": "all" if v[0] else "None", "out": "all" if v[1] else "None"}
+        # allow all subnets not explicitly disallowed? maybe a bad idea...
+        # subnetData["NACLs"][k] = {"in": "all", "out": "all"}
         return subnetData
 
     def _scene_to_scenario(self, scene: Scene) -> Scenario:
         sc: dict[str, dict[str, Any]] = {"Agents": {}, "Subnets": {}, "Hosts": {"Defender": {"image": "Velociraptor_Server"}}}
+        # FIXME this should not depend on object order
+        # also maybe calculate subnet size *after* everything gets added to it
         for obj in scene.objects:
             if isinstance(obj, CybORGSubnet) and obj.cyborg_name not in sc["Subnets"]:
                 sc["Subnets"][obj.cyborg_name] = self._initialize_subnet(obj)
             elif isinstance(obj, CybORGHost):
-                hostData = {"image": "Internal", "info": {obj.cyborg_name: "All"}}
-                # TODO image, confidentiality, availability
-                # could have subnet+image mapped to latter 2 per scenario
-                # for instance, subnet of unimportant servers but users with confidential documents
-                # vs a subnet of important servers and unimportant users
-                # TODO info interfaces
+                hostData = {"image": obj.image, "info": {obj.cyborg_name: {"Interfaces": "All"}}}
+                if obj.confidentiality:
+                    hostData["Confidentiality"] = obj.confidentiality
+                if obj.availability:
+                    hostData["Availability"] = obj.availability
                 # info is a dict mapping hostnames to a level of info that red acquires on compromising a host
                 # 'All' means red gets access to the host, while 'IP Address' just means that red becomes aware of the host without needing to scan
                 # This is used for lateral movement, so for every subnet that the red agent does not start in
                 # there should be at least one path back to the red agent's starting subnet following info links
+                # I don't think we need more here, a super control host is highly unlikely
+                for h in obj.linked_hosts:
+                    hostData["info"][h]["Interfaces"] = "IP Address"
                 if obj.subnet.cyborg_name not in sc["Subnets"]:
                     sc["Subnets"][obj.subnet.cyborg_name] = self._initialize_subnet(obj.subnet)
                 sc["Subnets"][obj.subnet.cyborg_name]["Size"] += 1
                 sc["Subnets"][obj.subnet.cyborg_name]["Hosts"].apppend(obj.cyborg_name)
                 sc["Hosts"][obj.cyborg_name] = hostData
             elif isinstance(obj, CybORGAgent):
-                agentData = {"agent_type": "SleepAgent", "Actions": ["Sleep"], "wrappers": [], "starting_sessions": [], "reward_calculator_type": None, "INT": {"Hosts": {}}, "AllowedSubnets": []}
+                agentData = {"agent_type": "SleepAgent", "Actions": ["Sleep"], "wrappers": [], "starting_sessions": [], "reward_calculator_type": "None", "INT": {"Hosts": {}}, "AllowedSubnets": []}
                 # TODO agent type, allowed subnets, actions, reward calcuator, initial access
                 if isinstance(obj, CybORGEgoAgent):
                     # First generate session for defender
-                    agentData["starting_sessions"].append({"name": "VeloServer", "username": "ubuntu", "type": "VelociraptorServer", "hostname": "Defender", "artifacts": []})
-                    # TODO artifacts
+                    agentData["starting_sessions"].append({"name": "VeloServer", "username": "ubuntu", "type": "VelociraptorServer", "hostname": "Defender", "artifacts": obj.artifacts})
                     # Then generate sessions for all hosts
-                    # We need a client for the defender unless the defender is in its own subnet, which doesn't have a flag yet
-                    # TODO map image names to usernames so that we can be SYSTEM on Windows
-                    # curiously we seem to use ubuntu on all linux systems instead of root
-                    for host in sc["Hosts"]:
+                    for host, d in sc["Hosts"]:
                         if host != "Attacker":
-                            agentData["starting_sessions"].append({"name": "Velo_" + host, "username": "ubuntu", "type": "VelociraptorClient", "hostname": host, "parent": "VeloServer"})
+                            agentData["starting_sessions"].append({"name": "Velo_" + host, "username": "SYSTEM" if d["image"].lower().startswity("win") else "ubuntu", "type": "VelociraptorClient", "hostname": host, "parent": "VeloServer"})
                             agentData["INT"]["Hosts"][host] = {"User info": "All", "System info": "All", "Interfaces": "All"}
                     agentData["AllowedSubnets"] = list(sc["Subnets"].keys())
-                    # TODO remove attacker subnet if present
                 else:
                     # Turn session tuple into the real deal
                     agentData["starting_sessions"].append(obj.session._asdict())
